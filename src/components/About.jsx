a9184b247a323cddbb75f51fc8f2cbf083929281
AkeKung <kingake555@gmail.com>
2020-03-20 23:51:30 +0700

    first

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,10 @@ export default function About() {
     <div>
       <div className="container mt-5">
         <div className="card">
-          <img src={process.env.PUBLIC_URL+'/andreas-klassen.jpg'} className='card-img-top' alt="..." />
+          <img src={"https://firebasestorage.googleapis.com/v0/b/lab08-expenses.appspot.com/o/andreas-klassen.jpg?alt=media&token=1aa68037-3993-4856-93e1-9c19c501416d"} className='card-img-top' alt="..." />
           <div className="card-body">
             <h5 className="card-title">Developer information</h5>
-            <p>Dome Potikanond (610610999)</p>
+            <p>Apichart Nutchanat (610610707)</p>
             <p>This app use Google Firebase as backend.</p>
             <Link to="/">
               <h3>
